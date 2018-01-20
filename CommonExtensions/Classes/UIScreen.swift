b82d0141da9e16f79140eb7f4c50cbb71e511758
Jerome Danthinne <jerome@grincheux.be>
2018-01-20 11:54:27 +0100

    UIScreen

---
+++ CommonExtensions/Classes/UIScreen.swift
@@ -0,0 +1,12 @@
+//
+//  UIScreen.swift
+//  CommonExtensions
+//
+//  Created by Jérôme Danthinne on 20/01/18.
+//
+
+extension UIScreen {
+    
+    public static let oneDevicePixel = 1 / UIScreen.main.scale
+    
+}
