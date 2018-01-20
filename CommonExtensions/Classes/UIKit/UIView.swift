52e5033521442985a77f69965ce499aab4d0351e
Jerome Danthinne <jerome@grincheux.be>
2018-01-20 16:52:11 +0100

    UIView: setBorder(color: UIColor, width: CGFloat = 1)

--- CommonExtensions/Classes/UIKit/UIView.swift
+++ CommonExtensions/Classes/UIKit/UIView.swift
@@ -7,6 +7,11 @@
 
 extension UIView {
     
+    public func setBorder(color: UIColor, width: CGFloat = 1) {
+        layer.borderWidth = width
+        layer.borderColor = color.cgColor
+    }
+    
     public func addRoundedBorder(color: UIColor, cornerRadius: CGFloat, lineDashPattern: [NSNumber]? = nil) {
         let rect = CGRect(x: 0.5, y: 0.5, width: self.bounds.width - 1, height: self.bounds.height - 1)
         let borderPath = UIBezierPath(roundedRect: rect, cornerRadius: cornerRadius).cgPath
