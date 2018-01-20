b86de36c7537a2d8f1877c99a1e4e1a5b9aaaa2b
Jerome Danthinne <jerome@grincheux.be>
2018-01-20 12:23:02 +0100

    Date: now

--- CommonExtensions/Classes/Foundation/Date.swift
+++ CommonExtensions/Classes/Foundation/Date.swift
@@ -7,6 +7,10 @@
 
 extension Date {
     
+    public static var now: Int {
+        return Int(Date().timeIntervalSince1970)
+    }
+    
     public var startOfDay: Date {
         return Calendar.current.startOfDay(for: self)
     }
