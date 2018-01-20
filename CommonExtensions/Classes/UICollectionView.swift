bc5f05f765e8e940e516c08575c4148faf9a7910
Jerome Danthinne <jerome@grincheux.be>
2018-01-20 11:54:20 +0100

    UICollectionView

---
+++ CommonExtensions/Classes/UICollectionView.swift
@@ -0,0 +1,18 @@
+//
+//  UICollectionView.swift
+//  CommonExtensions
+//
+//  Created by Jérôme Danthinne on 20/01/18.
+//
+
+extension UICollectionView {
+    
+    public func register(cellWithReuseIdentifiers names: [String]) {
+        names.forEach({ self.register(UINib(nibName: $0, bundle: nil), forCellWithReuseIdentifier: $0) })
+    }
+    
+    public func register(supplementaryViewOfKind kind: String, withReuseIdentifiers names: [String]) {
+        names.forEach({ self.register(UINib(nibName: $0, bundle: nil), forSupplementaryViewOfKind: kind, withReuseIdentifier: $0) })
+    }
+    
+}
