915b6ab406d4be9d7db2f749e6a4d4a4c56a57c2
Jerome Danthinne <jerome@grincheux.be>
2018-01-21 18:53:58 +0100

    UIPageViewController

---
+++ CommonExtensions/Classes/UIKit/UIPageViewController.swift
@@ -0,0 +1,27 @@
+//
+//  UIPageViewController.swift
+//  CommonExtensions
+//
+//  Created by Jérôme Danthinne on 21/01/18.
+//
+
+extension UIPageViewController {
+    
+    public func setViewControllersSafely(_ viewControllers: [UIViewController]?, direction: UIPageViewControllerNavigationDirection, animated: Bool, completion: ((Bool) -> Void)? = nil) {
+        guard animated else {
+            self.setViewControllers(viewControllers, direction: direction, animated: false, completion: completion)
+            return
+        }
+        
+        self.setViewControllers(viewControllers, direction: direction, animated: true) { finished in
+            if finished {
+                DispatchQueue.main.async {
+                    self.setViewControllers(viewControllers, direction: direction, animated: false, completion: completion)
+                }
+            } else if let completion = completion {
+                completion(finished)
+            }
+        }
+    }
+    
+}
