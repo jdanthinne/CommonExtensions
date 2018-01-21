1a5bcb99c9b33a630b2dcc44eaff6be21f69568d
Jerome Danthinne <jerome@grincheux.be>
2018-01-21 09:15:13 +0100

    I18n: iso code to languages names

--- CommonExtensions/Classes/Foundation/I18n.swift
+++ CommonExtensions/Classes/Foundation/I18n.swift
@@ -6,7 +6,12 @@
 //
 
 extension String {
-    
+
+    /**
+     Return a localized string
+     
+     :returns: No return value
+     */
     public var localized: String {
         return NSLocalizedString(self, comment: "")
     }
@@ -19,8 +24,25 @@ extension String {
         return String(format: localized, arg ?? "")
     }
     
+    /**
+     Return a localized string with multiple arguments
+     
+     :param: args   A list of arguments to be used as replacement in the localized string
+     
+     :returns: A localized string
+     */
     public func localizedWithArgs(_ args: [String]) -> String {
         return String(format: localized, arguments: args)
+    }
+    
+    public var isoToLanguageName: String {
+        let locale = Locale(identifier: Locale.current.languageCode!)
+        return (locale as NSLocale).displayName(forKey: NSLocale.Key.identifier, value: self)!
+    }
+    
+    public var isoToNativeLanguageName: String {
+        let locale = Locale(identifier: self)
+        return (locale as NSLocale).displayName(forKey: NSLocale.Key.identifier, value: self)!
     }
     
 }
