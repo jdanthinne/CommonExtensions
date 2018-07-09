cad055ae10adbba26e4af8918f7fb6fc403c9c83
Jerome Danthinne <jerome@grincheux.be>
2018-07-09 08:58:20 +0200

    UIViewController: add/remove childViewController

--- CommonExtensions/Classes/UIKit/UIViewController.swift
+++ CommonExtensions/Classes/UIKit/UIViewController.swift
@@ -39,6 +39,22 @@ extension UIApplication {
 
 extension UIViewController {
     
+    public func add(_ child: UIViewController) {
+        addChildViewController(child)
+        view.addSubview(child.view)
+        child.didMove(toParentViewController: self)
+    }
+    
+    public func remove() {
+        guard parent != nil else {
+            return
+        }
+        
+        willMove(toParentViewController: nil)
+        removeFromParentViewController()
+        view.removeFromSuperview()
+    }
+    
     public func handleKeyboardChanges() {
         if self is KeyboardChangesDelegate {
             NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChangeFrame(_:)), name: .UIKeyboardWillChangeFrame, object: nil)
