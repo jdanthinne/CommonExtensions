6c438eb9b0942c70b2dce5467deba262bf9b3651
Jerome Danthinne <jerome@grincheux.be>
2018-10-01 16:40:52 +0200

    UIColor: modified(hue…)

--- CommonExtensions.podspec
+++ CommonExtensions.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'CommonExtensions'
-  s.version          = '1.1.12'
+  s.version          = '1.1.13'
   s.summary          = 'Useful Swift UIKit extensions.'
 
 # This description is used to generate tags and improve search results.
--- CommonExtensions/Classes/UIKit/UIColor.swift
+++ CommonExtensions/Classes/UIKit/UIColor.swift
@@ -30,6 +30,7 @@ extension String {
 extension UIColor {
 
     public func add(_ overlay: UIColor) -> UIColor {
+        
         var bgR: CGFloat = 0
         var bgG: CGFloat = 0
         var bgB: CGFloat = 0
@@ -48,6 +49,23 @@ extension UIColor {
         let b = fgA * fgB + (1 - fgA) * bgB
 
         return UIColor(red: r, green: g, blue: b, alpha: 1.0)
+    }
+    
+    public func modified(withAdditionalHue hue: CGFloat, additionalSaturation: CGFloat, additionalBrightness: CGFloat) -> UIColor {
+        
+        var currentHue: CGFloat = 0.0
+        var currentSaturation: CGFloat = 0.0
+        var currentBrigthness: CGFloat = 0.0
+        var currentAlpha: CGFloat = 0.0
+        
+        if self.getHue(&currentHue, saturation: &currentSaturation, brightness: &currentBrigthness, alpha: &currentAlpha) {
+            return UIColor(hue: currentHue + hue,
+                           saturation: currentSaturation + additionalSaturation,
+                           brightness: currentBrigthness + additionalBrightness,
+                           alpha: currentAlpha)
+        } else {
+            return self
+        }
     }
 
 }
