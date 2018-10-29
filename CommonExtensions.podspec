21c66504ef1584fe14e850bcc6182205ec8e6f6c
Jerome Danthinne <jerome@grincheux.be>
2018-10-29 16:50:30 +0100

    UIButton.setBackgroundColor(_:for)

--- CommonExtensions.podspec
+++ CommonExtensions.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'CommonExtensions'
-  s.version          = '1.1.17'
+  s.version          = '1.1.18'
   s.summary          = 'Useful Swift UIKit extensions.'
 
 # This description is used to generate tags and improve search results.
--- CommonExtensions/Classes/UIKit/UIButton.swift
+++ CommonExtensions/Classes/UIKit/UIButton.swift
@@ -13,5 +13,18 @@ extension UIButton {
         layoutIfNeeded()
         UIView.setAnimationsEnabled(true)
     }
+        
+    public func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(color.cgColor)
+        context?.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+        
+        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        self.setBackgroundImage(colorImage, for: state)
+    }
 
 }
