0ea8f8e2e2d0f0e8a8f82ee283820adbceede5f2
Jérôme Danthinne <jerome@grincheux.be>
2019-08-20 16:02:59 +0200

    APPLICATION_EXTENSION_API_ONLY = YES

--- CommonExtensions/Classes/UIKit/UIApplication.swift
+++ CommonExtensions/Classes/UIKit/UIApplication.swift
@@ -9,6 +9,25 @@ import UIKit
 
 extension UIApplication {
     static public var globalTintColor: UIColor? {
-        return shared.keyWindow?.tintColor
+        return topViewController()?.view.window?.tintColor
+    }
+    
+    public class func topViewController(controller: UIViewController? = nil) -> UIViewController? {
+        if let navigationController = controller as? UINavigationController {
+            return topViewController(controller: navigationController.visibleViewController)
+        }
+        
+        if let tabController = controller as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return topViewController(controller: selected)
+            }
+        }
+        
+        if let presented = controller?.presentedViewController {
+            return topViewController(controller: presented)
+        }
+        
+        return controller
     }
 }
+
